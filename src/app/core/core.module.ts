7bd41c08674f3acf342ac8e818cbf8652ab60a16
Mariusz Targosz <mata@softwaremind.com>
2022-10-22 17:22:15 +0200

    Add core module with parent layout

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoreRoutingModule } from './core-routing.module';
+import { LayoutComponent } from './layout/layout.component';
+
+
+@NgModule({
+  declarations: [
+    LayoutComponent
+  ],
+  imports: [
+    CommonModule,
+    CoreRoutingModule
+  ]
+})
+export class CoreModule { }
