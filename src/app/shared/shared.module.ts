4f8e0124a25271dfb10b5695b74e41860cc1d7db
Mariusz Targosz <mata@softwaremind.com>
2022-10-22 23:22:26 +0200

    Add bootstrap

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '../material/material.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 const MODULES = [
   FormsModule,
+  NgbModule,
   MaterialModule
 ]
 
