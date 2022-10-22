c50108f645b8c2be5c9a4b77ef9046b249c5a586
Mariusz Targosz <mata@softwaremind.com>
2022-10-22 17:51:20 +0200

    Add configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Configuration } from "./configuration.models";
+
+export const environment: Configuration = {
+  production: true,
+  apiUrl: ''
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+import { Configuration } from "./configuration.models";
+
+export const environment: Configuration = {
+  production: false,
+  apiUrl: '',
 };
 
 /*
