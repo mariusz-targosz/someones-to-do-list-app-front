44deb72d507270539c6a1495406b6afd7acb3004
Mariusz Targosz <mata@softwaremind.com>
2022-10-24 20:38:35 +0200

    Add todo dialog service

---
+++ src/app/features/todos/dialogs/todos-dialogs.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Todo } from '../models/todo.model';
+import { EditTodoDialogComponent } from './edit-todo-dialog/edit-todo-dialog.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodosDialogsService {
+
+  constructor(private readonly _dialog: MatDialog) {
+  }
+
+  editTodo(todo: Todo): void {
+    this._dialog.open(EditTodoDialogComponent, {
+      data: { todo },
+    });
+  }
+}
